ea45b14a6bdfbed570ce8ebba4921e40bfe7bac2
ruspi <rus.pipan@ukr.net>
2021-02-20 16:59:08 +0200

    Minor changes.

--- src/main/java/mySpringContext/Main.java
+++ src/main/java/mySpringContext/Main.java
@@ -1,4 +1,19 @@
 package mySpringContext;
 
+import mySpringContext.model.AngryPolicemen;
+import mySpringContext.model.CoronaDisinfect;
+import mySpringContext.model.interfaces.Policemen;
+import mySpringContext.model.Room;
+import mySpringContext.service.Application;
+import mySpringContext.service.ApplicationContext;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = Application.run("mySpringContext", new HashMap<>(Map.of(Policemen.class, AngryPolicemen.class)));
+        CoronaDisinfect coronaDisinfect = context.getObject(CoronaDisinfect.class);
+        coronaDisinfect.start(new Room());
+    }
 }
